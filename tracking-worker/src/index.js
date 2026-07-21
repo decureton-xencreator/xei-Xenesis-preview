64a767de0c4f48b440246c31855c5f711a8104ba
decureton-xencreator <decureton@gmail.com>
2026-07-20 20:05:15 -0400

    Remove temporary Worker marker

--- tracking-worker/src/index.js
+++ tracking-worker/src/index.js
@@ -1,4 +1,3 @@
-// live test marker
 const EVENT_TYPES=new Set(['premiere_started','path_selected','premiere_completed','second_appointment_continued']);
 const PATHS=new Set(['bdc','company','compare']);
 const AUDIENCES=new Set(['ed','kim','ahmer']);
